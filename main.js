ccb09c2e47f07d3a4117e358b28b6bf87d25ad70
martypenner <penner.marty@gmail.com>
2013-12-10 12:29:42 -0500

    Ensure all files (not just the first) get saved (caused by core API filesystem API change). refs #2

--- main.js
+++ main.js
@@ -2,57 +2,76 @@
 /*global define, $, brackets, window */
 
 /**
- * Autosaves all files when leaving Brackets, a la PHPStorm.
+ * Autosaves all files when leaving Brackets, in the style of PHPStorm/WebStorm.
  *
- * The main sequential function to save files is essentially copied from _saveFileList()
- * in document/DocumentCommandHandlers.js. The only modification is a check if the current
- * document in the loop is untitled (i.e. hasn't been saved to a permanent disk location).
- * If it is, don't bother trying to save it.
+ * The functions are essentially copied from document/DocumentCommandHandlers.js. The only
+ * modification is a check if the current document in the loop is untitled (i.e. hasn't
+ * been saved to a permanent disk location). If it is, don't bother trying to save it.
  */
 define(function () {
-    "use strict";
+    'use strict';
     
-    var CommandManager = brackets.getModule("command/CommandManager"),
-        DocumentManager = brackets.getModule("document/DocumentManager"),
-        Async = brackets.getModule("utils/Async");
+    var CommandManager = brackets.getModule('command/CommandManager'),
+        Commands = brackets.getModule('command/Commands'),
+        DocumentManager = brackets.getModule('document/DocumentManager'),
+        Async = brackets.getModule('utils/Async');
 
     /** Unique token used to indicate user-driven cancellation of Save As (as opposed to file IO error) */
     var USER_CANCELED = { userCanceled: true };
     
-    $(window).on("blur", function () {
+     /**
+     * Saves all unsaved documents. Returns a Promise that will be resolved once ALL the save
+     * operations have been completed. If ANY save operation fails, an error dialog is immediately
+     * shown but after dismissing we continue saving the other files; after all files have been
+     * processed, the Promise is rejected if any ONE save operation failed (the error given is the
+     * first one encountered). If the user cancels any Save As dialog (for untitled files), the
+     * Promise is immediately rejected.
+     *
+     * @param {!Array.<File>} fileList
+     * @return {!$.Promise} Resolved with {!Array.<File>}, which may differ from 'fileList'
+     *      if any of the files were Unsaved documents. Or rejected with {?FileSystemError}.
+     */
+    function _saveFileList(fileList) {
         // Do in serial because doSave shows error UI for each file, and we don't want to stack
         // multiple dialogs on top of each other
-        var userCanceled = false;
-
+        var userCanceled = false,
+            filesAfterSave = [];
+            
         return Async.doSequentially(
-            DocumentManager.getWorkingSet(),
+            fileList,
             function (file) {
-                // Abort remaining saves if user canceled any Save dialog
+                // Abort remaining saves if user canceled any Save As dialog
                 if (userCanceled) {
                     return (new $.Deferred()).reject().promise();
                 }
-
+                
                 var doc = DocumentManager.getOpenDocumentForPath(file.fullPath);
-                if (doc && !doc.isUntitled()) {
-                    var savePromise = CommandManager.execute("file.save", {doc: doc});
+                if (doc) {
+                    var savePromise = CommandManager.execute(Commands.FILE_SAVE, {doc: doc});
                     savePromise
                         .done(function (newFile) {
-                            file.fullPath = newFile.fullPath;
-                            file.name = newFile.name;
+                            filesAfterSave.push(newFile);
                         })
                         .fail(function (error) {
                             if (error === USER_CANCELED) {
                                 userCanceled = true;
                             }
                         });
-
                     return savePromise;
                 } else {
                     // working set entry that was never actually opened - ignore
+                    filesAfterSave.push(file);
                     return (new $.Deferred()).resolve().promise();
                 }
             },
-            false
-        );
+            false  // if any save fails, continue trying to save other files anyway; then reject at end
+        ).then(function () {
+            return filesAfterSave;
+        });
+    }
+    
+    // Save all changed (but not untitled) documents when Brackets loses focus
+    $(window).on('blur', function () {
+        _saveFileList(DocumentManager.getWorkingSet());
     });
 });
